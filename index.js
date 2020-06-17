82d1ec4d02a1d63676b3d2ddb8c02b5135c61c24
my9756jjh <62217709+my9756jjh@users.noreply.github.com>
2020-06-17 09:47:05 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-function start(){
-alert("왜들어옴");
-alert("ㅋㅋ 낚였다는거 모르니);
-var x=0;
-var y = 1;
-while(y<1000){
-while(x<1000){
-alert(1000 - x + "번 더 클릭해보삼");
-x++
-}
-alert(y + "/1000쯤 옴");
-y++
-}
-}
+  alert("왜들어옴");
+  alert("ㅋㅋ 낚였다는거 모르니");
+  var x = 0;
+  var y = 1;
+  while (y < 1000) {
+    while (x < 1000) {
+      alert(1000 - x + "번 더 클릭해보삼");
+      x++;
+    }
+    alert(y + "/1000쯤 옴");
+    y++;
+    x = 0;
+  }
